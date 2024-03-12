bb424132e9fe665ee95a63a6221fc248e971ff36
jean.vasquez <poolvasquez.77@gmail.com>
2024-03-11 19:23:43 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
-import './App.css'
-import { Card } from './components/Card'
-import { ShowHide } from './components/ShowHide'
-import vehicles from './data/vehicles'
+import './App.css';
+import { Card } from './components/Card';
+import { ShowHide } from './components/ShowHide';
+import vehicles from './data/vehicles';
 
 function App() {
-
-  const vehicleList = vehicles.map(v => {
-    return <Card key={v.name} title={v.name} description={v.description} />
-  })
+  const vehicleList = vehicles.map((v) => {
+    return <Card key={v.name} title={v.name} description={v.description} />;
+  });
 
   return (
     <>
       <h1>Hola soy el padre</h1>
-      <div className="container">
-        {vehicleList}
-      </div>
+      <div className="container">{vehicleList}</div>
+      <div></div>
       <ShowHide />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
